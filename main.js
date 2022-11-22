9787fc952543dcbf449c30ac5640d023dc1589d2
TeaChareqishvili <teachareqishvili89@gmail.com>
2022-11-23 01:02:00 +0400

    section-1

---
+++ main.js
@@ -0,0 +1,15 @@
+
+const slideShow = document.querySelectorAll('.hide');
+let currentImageCounter = 0;
+
+ slideShow[currentImageCounter].style.display ="block";
+// slideShow[currentImageCounter].style.opacity=1;
+//   setInterval(nextImage, 5000);
+
+function nextImage(){
+     slideShow[currentImageCounter].style.display ="none";
+    // slideShow[currentImageCounter].style.opacity=0;
+    currentImageCounter= ( currentImageCounter+1)%slideShow.length;
+     slideShow[currentImageCounter].style.display ="block";
+    // slideShow[currentImageCounter].style.opacity=1;
+}
