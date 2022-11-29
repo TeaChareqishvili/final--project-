cf5f446ac742d90a43e727920c005a303cb4066a
TeaChareqishvili <teachareqishvili89@gmail.com>
2022-11-30 01:26:53 +0400

    fourth section

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const slideShow = document.querySelectorAll('.hide');
 let currentImageCounter = 0;
 
 slideShow[currentImageCounter].style.display ="block";
-//   setInterval(nextImage, 5000);
+   setInterval(nextImage, 5000);
 
 function nextImage(){
     slideShow[currentImageCounter].style.display ="none";
-    currentImageCounter= (currentImageCounter + 1)%(slideShow.length - 1);
+    currentImageCounter= (currentImageCounter + 1)%(slideShow.length);
     slideShow[currentImageCounter].style.display ="block";
 }
 
@@ -19,7 +19,7 @@ const Photoshop = document.getElementById('Photoshop');
 
 window.addEventListener('scroll', () => {
     if(skills.getBoundingClientRect().top <= 400) {
-        console.log('start animation')
+        // console.log('start animation')
         Design.style.width = 80 + "%"
         Photography.style.width = 50 + "%"
         Marking.style.width = 65 + "%"
@@ -27,51 +27,3 @@ window.addEventListener('scroll', () => {
     }
 })
 
-
-//Progress bar animation
-//end percent 
-//initial percent
-// const percentage = document.querySelector('.progress').innerHTML;
-// const percent = document.querySelector('.progress');
-// const initialPercent = 0;
-// let progress = 0;
-
-// const animate = (per, item)=> {
-//     if(progress == per) {
-//         window.removeEventListener('scroll')
-//         // clearInterval(loading)
-//     }else {
-//         progress++
-//         console.log(item)
-//         item.style.width = progress + "%"
-//     }
-// }
-
-// const handleLoading = (percentage, id  ) => {
-//     console.log('?')
-//     const item = document.getElementById(`${id}`);
-//     setInterval(() => {
-//             console.log('oe')
-//             animate(percentage, item)
-           
-//         }, 100)
-//     console.log({item})
-// }
-
-// const body = document.querySelector('.html');
-
-// window.addEventListener('scroll', () => {
-//     console.log('alloooo')
-// //    if( window.scrollTop > 50) {
-// //     console.log('alloooo2')
-
-//         handleLoading(80, 'Design')
-//         handleLoading(50, 'Photography')
-//         handleLoading(30, 'Marking')
-//         handleLoading(65, 'Photoshop')
-//     // }
-// })
-
-
-
-// handleLoading(80, 3)
